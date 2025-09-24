dbc205dbeb03273ef2d5842ea088a5ed3a52032d
Helio Silva <suporte.computek@gmail.com>
2025-09-24 01:24:10 -0300

    Adicionado rotina do tamanho do produto ser digitado

--- src/components/product-page/Header/SizeSelection.tsx
+++ src/components/product-page/Header/SizeSelection.tsx
@@ -4,37 +4,34 @@ import { setSizeSelection } from "@/lib/features/products/productsSlice";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks/redux";
 import { RootState } from "@/lib/store";
 import { cn } from "@/lib/utils";
-import React, { use, useEffect } from "react";
+import React, { use, useEffect, useState } from "react";
 
 const SizeSelection = ({ sizes }: { sizes: string[] }) => {
-  const { sizeSelection } = useAppSelector(
-    (state: RootState) => state.products
-  );
+  const [stTamanho, setStTamanho] = useState("");
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (sizes.length > 0) {
-      dispatch(setSizeSelection(sizes[0]));
+      dispatch(setSizeSelection(String(stTamanho)));
     }
-  }, [sizes]);
+  }, [stTamanho]);
 
   return (
     <div className="flex flex-col">
       <span className="text-sm sm:text-base text-black/60 mb-4">Tamanho</span>
       <div className="flex items-center flex-wrap lg:space-x-3">
-        {sizes.map((size: any, index: number) => (
-          <button
-            key={index}
-            type="button"
-            className={cn([
-              "bg-[#F0F0F0] flex items-center justify-center px-5 lg:px-6 py-2.5 lg:py-3 text-sm lg:text-base rounded-full m-1 lg:m-0 max-h-[46px]",
-              sizeSelection === size && "bg-[#685048] font-medium text-white",
-            ])}
-            onClick={() => dispatch(setSizeSelection(size))}
-          >
-            {size}
-          </button>
-        ))}
+      <input
+        placeholder="Digite o tamanho"
+        type="text"
+        inputMode="numeric"
+        pattern="[0-9]*"
+        value={stTamanho}
+        onChange={(e) => {
+          const onlyNums = e.target.value.replace(/\D/g, "");
+          setStTamanho(onlyNums);
+        }}
+        className={"w-full rounded-md border px-3 py-2 focus:outline-none focus:ring-2"}
+      />
       </div>
     </div>
   );
