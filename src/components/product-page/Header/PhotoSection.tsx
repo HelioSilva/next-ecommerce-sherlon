5dafa59890ed0b91b642821fff9975a5b9c297cb
klemerson01 <klemerson.sr@gmail.com>
2026-06-09 12:28:00 -0300

    Alteração na exibição de imagens

--- src/components/product-page/Header/PhotoSection.tsx
+++ src/components/product-page/Header/PhotoSection.tsx
@@ -11,19 +11,21 @@ import "react-medium-image-zoom/dist/styles.css";
 const PhotoSection = ({ data }: { data: Product }) => {
   const [selected, setSelected] = useState<string>(data.srcUrl);
 
+  const images = [data.srcUrl, ...(data.gallery?.map((p) => p.imagem) || [])];
+
   return (
     <div className="flex flex-col-reverse lg:flex-row lg:space-x-3.5">
       {data?.gallery && data.gallery.length > 0 && (
         <div className="flex lg:flex-col space-x-3 lg:space-x-0 lg:space-y-3.5 w-full lg:w-fit items-center lg:justify-start justify-center">
-          {data.gallery.map((photo, index) => (
+          {images.map((photo, index) => (
             <button
               key={index}
               type="button"
               className="bg-[#F0EEED] rounded-[13px] xl:rounded-[20px] w-full max-w-[111px] xl:max-w-[152px] max-h-[106px] xl:max-h-[167px] xl:min-h-[167px] aspect-square overflow-hidden"
-              onClick={() => setSelected(photo.imagem)}
+              onClick={() => setSelected(photo)}
             >
               <Image
-                src={photo.imagem}
+                src={photo}
                 width={152}
                 height={167}
                 className="rounded-md w-full h-full object-cover hover:scale-110 transition-all duration-500"
